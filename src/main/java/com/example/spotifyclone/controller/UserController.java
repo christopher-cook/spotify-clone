953afb868d1f324231445bd2f99271bba0f0efc6
Christopher Cook <cgcook81@gmail.com>
2019-10-29 15:20:20 -0400

    setup spring boot frame

---
+++ src/main/java/com/example/spotifyclone/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.spotifyclone.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
